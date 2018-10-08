f83ef4aebe8be86e30857db47b44a6bb03c54fc0
Extrasy <luisalexpm@gmail.com>
2018-10-07 20:44:38 -0700

    update

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -5,7 +5,7 @@ import { FaArrowRight } from 'react-icons/fa';
 //stateless component
 const Score = props => (
   <div className="gameScore">
-    <h3 className="score">Your Score < FaArrowRight /> {props.total}</h3>
+    <h3 className="score">- Score -< FaArrowRight /> {props.total}</h3>
     <h3 className="status">{props.status}</h3>
   </div>
 );
